09b03ce7510b0a83f6de36df4b2fd2cdbb264d42
lovodkin93 <57570615+lovodkin93@users.noreply.github.com>
2025-07-10 11:00:24 +0300

    Update bulma-carousel.js

--- static/js/bulma-carousel.js
+++ static/js/bulma-carousel.js
@@ -1,7 +1,3 @@
-/**
- * @license
- * SPDX-License-Identifier: Apache-2.0
- */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
